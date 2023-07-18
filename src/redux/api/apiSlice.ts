43e7ee8b8c7367ac49f8fc5bbc387ac3e40d0f33
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2023-07-18 13:40:18 +0600

    project redux api  file  added

---
+++ src/redux/api/apiSlice.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import Cookies from "js-cookie";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-catalog-server-site.vercel.app/",
+   // baseUrl: "http://localhost:5000/",
+    prepareHeaders: (headers) => {
+      const token = Cookies.get("token");
+      if (token) {
+        headers.set("Authorization", `${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["addNewBook", "bookDetails", "deleteBook", "bookReview"],
+  endpoints: () => ({}),
+});
